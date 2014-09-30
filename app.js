29d9c7727c39eaf251091312c49e902f5eaa4ced
Joel Skrepnek <jskrepnek@paybyphone.com>
2014-09-30 13:50:56 -0700

    Step 3

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'home.html',
+            controller: 'WidgetsController'
+        })
+        .when('/about', {
+            templateUrl: 'about.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
 
 app.controller('WidgetsController', ['$scope', '$http',
     function($scope, $http) {
