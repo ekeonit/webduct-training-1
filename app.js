bfa7039df80e389fbc1176ae79a262e4e75c8900
Joel Skrepnek <jskrepnek@paybyphone.com>
2014-09-30 13:33:06 -0700

    Step 2

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 var app = angular.module('app', []);
 
-app.controller('WidgetsController', ['$scope',
-    function($scope) {
-        $scope.widgets = [{
-            name: 'Dish Antenna',
-            description: 'Zonec id elit non mi porta gravida at eget metus.',
-            thumbNailSrc: 'Dish-Antenna-128.png',
-            cost: 45
-        }, {
-            name: 'Loud Speaker',
-            description: 'Borbi leo risus, porta ac consectetur ac, vestibulum at eros.',
-            thumbNailSrc: 'Loud-Speaker-128.png',
-            cost: 20
-        }, {
-            name: 'iPad',
-            description: 'Taecenas sed diam eget risus varius blandit sit amet non magna.',
-            thumbNailSrc: 'iPad-128.png',
-            cost: 56
-        }];
+app.controller('WidgetsController', ['$scope', '$http',
+    function($scope, $http) {
+        $scope.widgets = [];
+        $http.get('data/widgets.json')
+            .success(function(result) {
+                $scope.widgets = result;
+            })
+            .error(function(result) {
+                alert('Error');
+            });
 
         $scope.orderByField = 'name';
     }
